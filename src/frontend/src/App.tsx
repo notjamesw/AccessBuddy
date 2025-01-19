1159aceb18a4a435152e3e71d35f1b3e9e2e561c
jameswen <leranjamesw@gmail.com>
2025-01-18 20:03:53 -0800

    frontend template

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import './Newtab.css';
-import './Newtab.scss';
+import logo from './logo.svg';
+import './App.css';
 
-const Newtab = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,12 +16,11 @@ const Newtab = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React
         </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
       </header>
     </div>
   );
-};
+}
 
-export default Newtab;
+export default App;
