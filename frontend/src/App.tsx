b3345d9de9a04a439568230259549a49de9b096c
Jiayang Dong <dongjiayang123@hotmail.com>
2025-01-19 13:48:03 -0800

    updated backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home.tsx';
 import Info from './components/Info.tsx';
@@ -8,14 +8,24 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="flex pt-4 justify-center space-x-32 bg-slate-900 text-white font-semibold text-md shadow-md">
-          <Link className = "ml-4 shadow-xl" to="/">
-            <img className = "active:brightness-50" src="/images/accessBuddy1.png" alt="Home" />
+        <nav className="flex justify-between items-center pt-8 bg-slate-900 text-white font-semibold text-md shadow-md h-16 px-6 md:px-10">
+          <Link to="/">
+            <img
+              className="h-16 active:brightness-50"
+              src="/images/accessBuddy1.png"
+              alt="Home"
+            />
           </Link>
-          <Link style={{ marginRight: '1rem' }} to="/info">
-            <img className = "rounded-full bg-slate-800 hover:brightness-150 hover:ring active:bg-slate-900" src="/images/infoIcon.png" alt="About" />
-          </Link>
-        </nav>  
+          <div className="flex space-x-6">
+            <Link to="/info">
+              <img
+                className="h-8 w-8 rounded-full bg-slate-800 hover:brightness-150 hover:ring active:bg-slate-900"
+                src="/images/infoIcon.png"
+                alt="About"
+              />
+            </Link>
+          </div>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/info" element={<Info />} />
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,7 +3,7 @@ import VoiceRecorder from "./VoiceRecorder.tsx";
 
 const Home = () => {
     return (
-        <div className = "flex-rows text-xl min-w-80 min-h-80 bg-slate-900 text-white flex items-center justify-center">
+        <div className="flex-rows text-xl min-w-80 min-h-80 bg-slate-900 text-white flex items-center justify-center">
             <VoiceRecorder />
         </div>
     );
